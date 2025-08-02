2a3c0d8ba1c8b2d19fda56091e3fdf675522941d
skyhunter12 <akashbagali043@gmail.com>
2025-08-02 15:58:04 +0530

    headers in app.js using helmet

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Checkout from "./routes/check-out/check-out.component";
 import Home from "./routes/home/home.component";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { checkUserSession } from "./routes/store/user/user.action";
+import { checkGoogleRedirectResult, checkUserSession } from "./routes/store/user/user.action";
+import { Helmet } from 'react-helmet';
+
 const App = () => {
   const dispatch = useDispatch();
 
@@ -16,6 +18,11 @@ const App = () => {
     dispatch(checkUserSession());
   }, [dispatch]);
   return (
+    <>
+    <Helmet>
+        <meta httpEquiv="Cross-Origin-Opener-Policy" content="unsafe-none" />
+        <meta httpEquiv="Cross-Origin-Embedder-Policy" content="unsafe-none" />
+    </Helmet>
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
@@ -25,6 +32,7 @@ const App = () => {
         <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
+    </>
   );
 };
 
